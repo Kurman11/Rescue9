556f89857e5e96e33aa9f40081651a9a0aab6aa8
Kurman11 <skedyd45@naver.com>
2023-05-10 10:26:26 +0900

    Fixed project name

--- rescue/wsgi.py
+++ rescue/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pjt project.
+WSGI config for rescue project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pjt.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rescue.settings')
 
 application = get_wsgi_application()
