24fbea6b61392d27ccc498a119f61ba8625aa660
SungBum Ha <gktjd425@naver.com>
2023-05-11 23:00:10 +0900

    추가-custom_ckeditor로 재정의

--- recipes/views.py
+++ recipes/views.py
@@ -163,3 +163,26 @@ def comment_like(request, recipe_pk, comment_pk):
     else:
         comment.like_users.add(request.user)
     return redirect('recipes:detail', recipe_pk)
+
+
+
+
+from django.http import Http404
+from django.http import JsonResponse
+from PIL import Image
+
+from django_ckeditor_5.views import image_verify, NoImageException, handle_uploaded_file
+from django_ckeditor_5.forms import UploadFileForm
+
+
+def upload_file(request):
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        try:
+            image_verify(request.FILES["upload"])
+        except NoImageException as ex:
+            return JsonResponse({"error": {"message": f"{str(ex)}"}})
+        if form.is_valid():
+            url = handle_uploaded_file(request.FILES["upload"])
+            return JsonResponse({"url": url})
+    raise Http404(_("Page not found."))
