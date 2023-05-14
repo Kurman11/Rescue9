17b176e5b18d76d4863e34d028f39671dea96d86
Choi-jw-96 <121466845+Choi-jw-96@users.noreply.github.com>
2023-05-14 14:41:45 +0900

    수정-profile

--- static/js/accounts_profile_follow.js
+++ static/js/accounts_profile_follow.js
@@ -11,17 +11,23 @@ form.addEventListener('submit', function (event) {
     headers: {'X-CSRFToken': csrftoken},
   })
     .then((response) => {
+      console.log(response.data.is_followed)
       const isFollowed = response.data.is_followed
       const followBtn = document.querySelector('#follow-form > input[type=submit]')
+      console.log(followBtn)
+
 
       if (isFollowed === true) {
-        followBtn.value = '언팔로우'
+        followBtn.value = 'Unfollow'
       } else {
-        followBtn.value = '팔로우'
+        followBtn.value = 'Follow'
       }
 
       const followingCountTag = document.querySelector('#followings-count')
       const followerCountTag = document.querySelector('#followers-count')
+
+      console.log(followingCountTag)
+      console.log(followerCountTag )
 
       const followingsCountData = response.data.followings_count
       const followersCountData = response.data.followers_count
