25d62ac6ff36e6388741819d854ffed39145a010
illson97 <illson97@github.com>
2023-05-18 17:12:16 +0900

    추가-소셜로그인 호스트 주소 추가

--- rescue/settings.py
+++ rescue/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['port-0-rescue9-17xqnr2algn95y3r.sel3.cloudtype.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
